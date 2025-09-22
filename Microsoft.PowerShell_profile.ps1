cccf743eb299b92825be6d28adc26d2b2dc5785a
Joséphine Barthel <jbarthel@amplitude-studios.com>
2025-09-22 12:15:13 +0200

    Edit Colors

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,4 @@
-oh-my-posh --init --shell pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/p10k_classic.omp.json | Invoke-Expression
+oh-my-posh --init --shell pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/catppuccin_mocha.omp.json | Invoke-Expression
 
 # Import the Chocolatey Profile that contains the necessary code to enable
 # tab-completions to function for `choco`.
@@ -11,7 +11,11 @@ if (Test-Path($ChocolateyProfile)) {
 }
 
 Import-Module PSReadLine
-Set-PSReadLineOption -Colors @{ "Default"="green" }
-Set-PSReadLineOption -Colors @{ "Parameter"="Blue" }
-Set-PSReadLineOption -Colors @{ "ContinuationPrompt"="yellow"}
-Set-PSReadLineOption -Colors @{ "Operator"="green" }   
+Set-PSReadLineOption -Colors @{ "Default"="cyan" }
+Set-PSReadLineOption -Colors @{ "Parameter"="white" }
+Set-PSReadLineOption -Colors @{ "ContinuationPrompt"="cyan"}
+Set-PSReadLineOption -Colors @{ "Operator"="yellow" }   
+Set-PSReadLineOption -Colors @{ "Emphasis"="red" }
+Set-PSReadLineOption -Colors @{ "Number"="white" }
+Set-PSReadLineOption -Colors @{ "String"="green" }
+Set-PSReadLineOption -Colors @{ "Command"="cyan"}
