0c592fa52868c2c7555a1a3977eee84b709dd13d
Jo Barthel <josephine.barthel11@gmail.com>
2025-10-14 13:00:32 +0200

    Better colors

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,15 +1,5 @@
 oh-my-posh --init --shell pwsh --config ~/AppData/Local/Programs/oh-my-posh/themes/catppuccin_mocha.omp.json | Invoke-Expression
 
-# Import the Chocolatey Profile that contains the necessary code to enable
-# tab-completions to function for `choco`.
-# Be aware that if you are missing these lines from your profile, tab completion
-# for `choco` will not function.
-# See https://ch0.co/tab-completion for details.
-$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
-if (Test-Path($ChocolateyProfile)) {
-  Import-Module "$ChocolateyProfile"
-}
-
 Import-Module PSReadLine
 Set-PSReadLineOption -Colors @{ "Default"="cyan" }
 Set-PSReadLineOption -Colors @{ "Parameter"="white" }
