8af46279e004cfd720f573e76d5ef77aaff2e3d3
Jo Barthel <josephine.barthel11@gmail.com>
2025-10-25 11:06:00 +0200

    mute beeps, green command highlight

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -14,7 +14,10 @@ Set-PSReadLineOption -Colors @{ "Operator"="yellow" }
 Set-PSReadLineOption -Colors @{ "Emphasis"="red" }
 Set-PSReadLineOption -Colors @{ "Number"="white" }
 Set-PSReadLineOption -Colors @{ "String"="green" }
-Set-PSReadLineOption -Colors @{ "Command"="cyan"}
+Set-PSReadLineOption -Colors @{ "Command"="green"}
+
+# Mute irritating beeps
+Set-PSReadlineOption -BellStyle None
 
 # More readable color for directories
 $PSStyle.FileInfo.Directory = "`e[31;1m"
